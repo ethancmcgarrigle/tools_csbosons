b0a967fd8a4bd8c303e23aab5f3fa7a2c95e6589
Ethan McGarrigle <ethancmcg@gmail.com>
2025-03-05 17:22:16 -0800

    update dynamical particle number script

--- keldysh_dynamics_scripts/plot_dynamical_particle_number.py
+++ keldysh_dynamics_scripts/plot_dynamical_particle_number.py
@@ -49,8 +49,14 @@ CL_time = cols[2]
 eqb_partnum = cols[3]
 eqb_partnum_im = cols[4]
 
-N_avg_eqb = np.mean(eqb_partnum)
-N_avg_eqb_err = stats.sem(eqb_partnum)
+
+# Averaging CL sampling data:
+pcnt_to_remove = 0.25
+warmup_samples = int(pcnt_to_remove * len(cols[0])) # num samples to be thrown out!  
+
+print('Averaging ' + str(len(cols[0]) - warmup_samples) + ' CL samples' )
+N_avg_eqb = np.mean(eqb_partnum[warmup_samples:])
+N_avg_eqb_err = stats.sem(eqb_partnum[warmup_samples:])
 
 # Get the t range; the first 5 columns are non-dynamical particle number info. 
 first_indx = 5
@@ -62,9 +68,11 @@ dynamical_partnum = np.zeros(num_tpoints, dtype=np.complex128)
 dynamical_partnum_err = np.zeros(num_tpoints, dtype=np.complex128)
 
 for i in range(num_tpoints): 
-  dynamical_partnum[i] = np.mean(cols[first_indx + 2*i] + 1j*cols[first_indx + 2*i] ) 
-  dynamical_partnum_err[i] = stats.sem(cols[first_indx + 2*i] + 1j*cols[first_indx + 2*i] ) 
+  dynamical_partnum[i] = np.mean(cols[first_indx + 2*i][warmup_samples:] + 1j*cols[first_indx + 2*i + 1][warmup_samples:] ) 
+  dynamical_partnum_err[i] = stats.sem(cols[first_indx + 2*i][warmup_samples:] + 1j*cols[first_indx + 2*i + 1][warmup_samples:] ) 
 
+
+savefig = True
 
 plt.style.use('~/CSBosonsCpp/tools/python_plot_styles_examples/plot_style_data.txt')
 
@@ -80,6 +88,8 @@ plt.title(title_str, fontsize = 16, fontweight = 'bold')
 plt.xlabel('Langevin Time', fontsize = 22)
 plt.ylabel(r'Eqb. Particle Number', fontsize = 20)
 plt.legend()
+if(savefig):
+  plt.savefig('N_eqb_plot.pdf', dpi=300)
 plt.show()
 
 
@@ -96,20 +106,31 @@ plt.show()
  #plt.show()
 
 
+# plot limits: 
+if(N_avg_eqb < 150.):
+  p_width = 2. * N_avg_eqb 
+else:
+  p_width = 0.1 * N_avg_eqb
+
+showImPart = True
 plt.figure(figsize = (4,4))
 plt.errorbar(real_time, dynamical_partnum.real, dynamical_partnum_err.real, color = 'b', linewidth=1.2, elinewidth = 1.2, markersize = 1, label = r'Re[$N$]')
 plt.axhline(y = N_avg_eqb, color = 'k', linestyle = 'solid', linewidth=1.2, label = r'Equilibrium')
+if(showImPart):
+  plt.errorbar(real_time, dynamical_partnum.imag, np.abs(dynamical_partnum_err), color = 'k', linewidth=1.2, elinewidth = 1.2, markersize = 1, label = r'Im[$N$]')
 axes = plt.gca()
 axes.fill_between(real_time, N_avg_eqb - N_avg_eqb_err, N_avg_eqb + N_avg_eqb_err, color = 'k', alpha = 0.5) 
 plt.title(title_str, fontsize = 20, fontweight = 'bold')
 plt.xlabel('Real time', fontsize = 24)
 plt.ylabel(r'Dynamical Particle Number', fontsize = 20)
 plt.legend()
-plt.ylim(N_avg_eqb - 0.1*N_avg_eqb, N_avg_eqb + 0.1*N_avg_eqb)
+plt.ylim(N_avg_eqb - p_width, N_avg_eqb + p_width) 
+if(savefig):
+  plt.savefig('N_t_plot.pdf', dpi=300)
 plt.show()
 
 # Sample and plot every N points 
-N = 1000
+N = 100
 plt.figure(figsize = (4,4))
 plt.errorbar(real_time[::N], dynamical_partnum[::N].real, dynamical_partnum_err[::N].real, color = 'b', linewidth=1.2, elinewidth = 1.2, markersize = 1, label = r'Re[$N$]')
 plt.axhline(y = N_avg_eqb, color = 'k', linestyle = 'solid', linewidth=1.2, label = r'Equilibrium')
@@ -118,7 +139,7 @@ axes.fill_between(real_time, N_avg_eqb - N_avg_eqb_err, N_avg_eqb + N_avg_eqb_er
 plt.title('Dynamical particle number', fontsize = 20, fontweight = 'bold')
 plt.xlabel('Real time', fontsize = 24)
 plt.ylabel(r'$N$', fontsize = 24)
-plt.ylim(N_avg_eqb - 0.1*N_avg_eqb, N_avg_eqb + 0.1*N_avg_eqb)
+plt.ylim(N_avg_eqb - p_width, N_avg_eqb + p_width) 
 plt.legend()
 plt.show()
 
